c7b1d5cf4fd36dd87cf460c4e3f0851c84337231
chris_mlvz <chris.mlvz@gmail.com>
2021-09-30 18:27:54 -0500

    Conditional Loops with while

--- the_book/common_concepts/control_flow/loops/src/main.rs
+++ the_book/common_concepts/control_flow/loops/src/main.rs
@@ -6,16 +6,28 @@
 // }
 
 // * Returning Values from Loops
+// fn main() {
+//     let mut counter = 0;
+
+//     let result = loop {
+//         counter += 1;
+
+//         if counter == 10 {
+//             break counter * 2;
+//         }
+//     };
+
+//     println!("The result is {}", result);
+// }
+
+// * Conditional Loops with while
 fn main() {
-    let mut counter = 0;
+    let mut number = 3;
+    while number != 0 {
+        println!("{}", number);
 
-    let result = loop {
-        counter += 1;
+        number -= 1;
+    }
 
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-
-    println!("The result is {}", result);
+    println!("LIFTOFF!!!");
 }
