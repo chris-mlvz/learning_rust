af52fb0e0f4a47c9925c0441b9880834e4286ea3
chris_mlvz <chris.mlvz@gmail.com>
2021-10-01 17:56:38 -0500

    Matching with Option<T>

--- the_book/enums_and_pattern_matching/match_operator/src/main.rs
+++ the_book/enums_and_pattern_matching/match_operator/src/main.rs
@@ -1,34 +1,48 @@
 // *The match Control Flow Operator
 
+// fn main() {
+//     println!("{}", value_in_cents(Coin::Penny));
+//     println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)));
+// }
+
+// #[derive(Debug)]
+// enum UsState {
+//     Alabama,
+//     Alaska,
+// }
+
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter(UsState),
+// }
+
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => {
+//             println!("Lucky penny!");
+//             1
+//         }
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter(state) => {
+//             println!("State quarter from {:?}!", state);
+//             25
+//         }
+//     }
+// }
+
+// * Matching with Option<T>
 fn main() {
-    println!("{}", value_in_cents(Coin::Penny));
-    println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)));
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
 
-#[derive(Debug)]
-enum UsState {
-    Alabama,
-    Alaska,
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        }
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        }
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
