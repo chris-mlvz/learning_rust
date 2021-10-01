cf8b0ba9a43aef6fb732a79fae3f4f38c11053e4
chris_mlvz <chris.mlvz@gmail.com>
2021-10-01 01:46:29 -0500

    fix error

--- the_book/structs/defining_structs/src/main.rs
+++ the_book/structs/defining_structs/src/main.rs
@@ -1,31 +1,31 @@
 // * Defining and Instantiating Structs
-// struct User {
-//     username: String,
-//     email: String,
-//     sign_in_count: u64,
-//     active: bool,
-// }
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
 
-// fn main() {
-//     let user1 = build_user(String::from("mail"), String::from("name"));
-//     // * Creating Instances From Other Instances With Struct Update Syntax
-//     let user2 = User {
-//         email: String::from("another@example.com"),
-//         username: String::from("anotherusername567"),
-//         ..user1
-//     };
-//     println!("{}", user1.active);
-// }
+fn main() {
+    let user1 = build_user(String::from("mail"), String::from("name"));
+    // * Creating Instances From Other Instances With Struct Update Syntax
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1
+    };
+    println!("{}", user1.active);
+}
 
 // * Using the Field Init Shorthand when Variables and Fields Have the Same Name
-// fn build_user(email: String, username: String) -> User {
-//     User {
-//         email,
-//         username,
-//         active: true,
-//         sign_in_count: 1,
-//     }
-// }
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
 
 // * Using Tuple Structs without Named Fields to Create Different Types
 // fn main() {
