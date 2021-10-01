d50f66d4e2a3076f818784c8a1e5c83fb6beba27
chris_mlvz <chris.mlvz@gmail.com>
2021-10-01 18:03:50 -0500

    The _ Placeholder

--- the_book/enums_and_pattern_matching/match_operator/src/main.rs
+++ the_book/enums_and_pattern_matching/match_operator/src/main.rs
@@ -34,15 +34,27 @@
 // }
 
 // * Matching with Option<T>
-fn main() {
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-}
+// fn main() {
+//     let five = Some(5);
+//     let six = plus_one(five);
+//     let none = plus_one(None);
+// }
 
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
+// fn plus_one(x: Option<i32>) -> Option<i32> {
+//     match x {
+//         None => None,
+//         Some(i) => Some(i + 1),
+//     }
+// }
+
+// * The _ Placeholder
+fn main() {
+    let some_u8_value = 1u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
     }
 }
