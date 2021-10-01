e98cecd991455ffe15b4b2ade2be39f101725780
chris_mlvz <chris.mlvz@gmail.com>
2021-10-01 15:50:36 -0500

    The match Control Flow Operator

---
+++ the_book/enums_and_pattern_matching/match_operator/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    println!("{}", value_in_cents(Coin::Penny));
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
