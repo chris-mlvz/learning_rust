7f9fbfbae1fe591a9026a5d639b7a5db85711c0c
chris_mlvz <chris.mlvz@gmail.com>
2021-10-01 15:26:46 -0500

    Methods on enums

--- the_book/enums_and_pattern_matching/enums/src/main.rs
+++ the_book/enums_and_pattern_matching/enums/src/main.rs
@@ -1,10 +1,31 @@
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
+// * Defining an Enum
+// enum IpAddr {
+//     V4(u8, u8, u8, u8),
+//     V6(String),
+// }
+
+// fn main() {
+//     let home = IpAddr::V4(127, 0, 0, 1);
+
+//     let loopback = IpAddr::V6(String::from("::1"));
+// }
+
+// * another example
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+// * Methods on  enums
+impl Message {
+    fn call(&self) {
+        println!("{}", "hi");
+    }
 }
 
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-
-    let loopback = IpAddr::V6(String::from("::1"));
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
