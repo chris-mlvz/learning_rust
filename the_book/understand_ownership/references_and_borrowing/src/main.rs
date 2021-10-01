1fe3e7fb8fbdb8a6ee63d39f4689f5b943f4de80
chris_mlvz <chris.mlvz@gmail.com>
2021-09-30 20:00:48 -0500

    Mutable References

--- the_book/understand_ownership/references_and_borrowing/src/main.rs
+++ the_book/understand_ownership/references_and_borrowing/src/main.rs
@@ -1,16 +1,33 @@
 // * References and Borrowing
 
-fn main() {
-    let s1 = String::from("hello");
+// fn main() {
+//     let s1 = String::from("hello");
 
-    let len = calculate_length(&s1);
+//     let len = calculate_length(&s1);
 
-    println!("The length of '{}' is {}", s1, len);
+//     println!("The length of '{}' is {}", s1, len);
 
-}
+// }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
-}
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
+// }
 
 // * Modify borrowing
+fn main() {
+    let mut s = String::from("hello");
+    change(&mut s);
+
+    {
+        let r1 = &mut s;
+        println!("{}", r1);
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let r2 = &mut s;
+
+    println!("{}", r2);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
