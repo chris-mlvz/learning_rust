3488a99769ca2529a0490f888adc9506c144384e
chris_mlvz <chris.mlvz@gmail.com>
2021-09-30 20:03:14 -0500

    data races

--- the_book/understand_ownership/references_and_borrowing/src/main.rs
+++ the_book/understand_ownership/references_and_borrowing/src/main.rs
@@ -14,20 +14,32 @@
 // }
 
 // * Modify borrowing
+// fn main() {
+//     let mut s = String::from("hello");
+//     change(&mut s);
+
+//     {
+//         let r1 = &mut s;
+//         println!("{}", r1);
+//     } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+//     let r2 = &mut s;
+
+//     println!("{}", r2);
+// }
+
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
+
 fn main() {
     let mut s = String::from("hello");
-    change(&mut s);
 
-    {
-        let r1 = &mut s;
-        println!("{}", r1);
-    } // r1 goes out of scope here, so we can make a new reference with no problems.
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
 
-    let r2 = &mut s;
-
-    println!("{}", r2);
-}
-
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
