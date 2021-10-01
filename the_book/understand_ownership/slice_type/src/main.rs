80aea574801c7e2e18e32bf4186ccde264aa71e3
chris_mlvz <chris.mlvz@gmail.com>
2021-10-01 01:04:21 -0500

    other slices

--- the_book/understand_ownership/slice_type/src/main.rs
+++ the_book/understand_ownership/slice_type/src/main.rs
@@ -41,22 +41,29 @@
 
 // * first_word with string slice
 
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
+// fn first_word(s: &str) -> &str {
+//     let bytes = s.as_bytes();
 
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[..i];
-        }
-    }
-    &s[..]
-}
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return &s[..i];
+//         }
+//     }
+//     &s[..]
+// }
 
+// fn main() {
+//     let mut s = String::from("hello world");
+
+//     let word = first_word(&s);
+//     println!("the first word is: {}", word);
+
+//     s.clear(); // error!
+// }
+
+// * Other slices
 fn main() {
-    let mut s = String::from("hello world");
-
-    let word = first_word(&s);
-    println!("the first word is: {}", word);
-
-    s.clear(); // error!
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[2, 3]);
 }
