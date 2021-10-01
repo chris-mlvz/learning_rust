31db898b9bd6a9d49c96a1611b50171d82f19f94
chris_mlvz <chris.mlvz@gmail.com>
2021-10-01 01:29:01 -0500

    Defining and Instantiating Structs

---
+++ the_book/structs/defining_structs/src/main.rs
@@ -0,0 +1,25 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn main() {
+    let user1 = build_user(String::from("mail"), String::from("name"));
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1
+    };
+    println!("{}", user1.active);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
