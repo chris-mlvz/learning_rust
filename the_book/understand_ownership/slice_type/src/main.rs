41716cefe833b66907709e6c6c69310f3514697a
chris_mlvz <chris.mlvz@gmail.com>
2021-10-01 01:01:02 -0500

    String Literals Are Slices

--- the_book/understand_ownership/slice_type/src/main.rs
+++ the_book/understand_ownership/slice_type/src/main.rs
@@ -1,22 +1,62 @@
-fn main() {
-    let mut s = String::from("hello world");
+// fn main() {
+//     let mut s = String::from("hello world");
 
-    let word = first_word(&s); // word will get the value 5
+//     let word = first_word(&s); // word will get the value 5
 
-    s.clear(); // this empties the String, making it equal to ""
+//     s.clear(); // this empties the String, making it equal to ""
 
-    // word still has the value 5 here, but there's no more string that
-    // we could meaningfully use the value 5 with. word is now totally invalid!
-    println!("{},{}", s, word);
-}
-fn first_word(s: &String) -> usize {
+//     // word still has the value 5 here, but there's no more string that
+//     // we could meaningfully use the value 5 with. word is now totally invalid!
+//     println!("{},{}", s, word);
+// }
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
+
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
+
+//     s.len()
+// }
+
+// * String slices
+
+// fn main() {
+//     // let s = String::from("hello world");
+
+//     // let hello = &s[0..5];
+//     // let world = &s[6..11];
+
+//     // println!("{},{},{}", s, hello, world);
+
+//     let s = String::from("hello");
+
+//     // let slice = &s[..2];
+//     // let slice = &s[3..];
+//     let slice = &s[..];
+//     println!("{},{}", s, slice);
+// }
+
+// * first_word with string slice
+
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[..i];
         }
     }
+    &s[..]
+}
 
-    s.len()
+fn main() {
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+    println!("the first word is: {}", word);
+
+    s.clear(); // error!
 }
