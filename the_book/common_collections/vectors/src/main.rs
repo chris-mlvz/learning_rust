5f13bf120016af10aaba44ffa55209c6ec705031
chris_mlvz <chris.mlvz@gmail.com>
2021-10-03 01:19:57 -0500

    Updating a vector

--- the_book/common_collections/vectors/src/main.rs
+++ the_book/common_collections/vectors/src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
-    let v: Vec<i32> = Vec::new();
-    let vector = vec![1, 2, 3];
+    // * Creating a New Vector
+    // let v: Vec<i32> = Vec::new();
+    // let vector = vec![1, 2, 3];
+
+    // * Updating a Vector
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+    println!("{:?}", v);
 }
