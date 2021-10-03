eb0d89f09bdf822819415d073e09ed26921a9eff
chris_mlvz <chris.mlvz@gmail.com>
2021-10-02 22:35:50 -0500

    Making Enums Public

--- the_book/packages_crates_modules/restaurant/src/lib.rs
+++ the_book/packages_crates_modules/restaurant/src/lib.rs
@@ -25,24 +25,37 @@
 // }
 
 // * Making Structs Public
-mod back_of_house {
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
+// mod back_of_house {
+//     pub struct Breakfast {
+//         pub toast: String,
+//         seasonal_fruit: String,
+//     }
 
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
+//     impl Breakfast {
+//         pub fn summer(toast: &str) -> Breakfast {
+//             Breakfast {
+//                 toast: String::from(toast),
+//                 seasonal_fruit: String::from("peaches"),
+//             }
+//         }
+//     }
+// }
+
+// pub fn eat_at_restaurant() {
+//     let mut meal = back_of_house::Breakfast::summer("Rye");
+//     meal.toast = String::from("Wheat");
+//     println!("I'd like {} toast please", meal.toast);
+// }
+
+// * Making Enums Public
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
     }
 }
 
 pub fn eat_at_restaurant() {
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
