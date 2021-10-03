7ac87744f29d85eadb642a9409fa7fdb9589c005
chris_mlvz <chris.mlvz@gmail.com>
2021-10-03 02:02:52 -0500

    Reading Elements of Vectors

--- the_book/common_collections/vectors/src/main.rs
+++ the_book/common_collections/vectors/src/main.rs
@@ -4,10 +4,36 @@ fn main() {
     // let vector = vec![1, 2, 3];
 
     // * Updating a Vector
-    let mut v = Vec::new();
-    v.push(5);
+    // let mut v = Vec::new();
+    // v.push(5);
+    // v.push(6);
+    // v.push(7);
+    // v.push(8);
+    // println!("{:?}", v);
+
+    // * Reading elements of Vectors
+    // let v = vec![1, 2, 3, 4, 5];
+
+    // let third: &i32 = &v[2];
+    // println!("The third element is {}", third);
+
+    // match v.get(2) {
+    //     Some(third) => println!("The third element is {}", third),
+    //     None => println!("There is not third element"),
+    // }
+
+    // * panic and None
+    // let v = vec![1, 2, 3, 4, 5];
+
+    // let does_not_exit = &v[100];
+    // let does_not_exit = v.get(100);
+
+    // * inmutable borrow
+    let mut v = vec![1, 2, 3, 4, 5];
+
+    let first = &v[0];
+
     v.push(6);
-    v.push(7);
-    v.push(8);
-    println!("{:?}", v);
+
+    println!("The first element is: {}", first);
 }
