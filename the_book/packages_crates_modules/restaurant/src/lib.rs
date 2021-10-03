f88961f4097428fd4349e23f8d03a761f9768f53
chris_mlvz <chris.mlvz@gmail.com>
2021-10-02 22:32:58 -0500

    Making Structs Public

--- the_book/packages_crates_modules/restaurant/src/lib.rs
+++ the_book/packages_crates_modules/restaurant/src/lib.rs
@@ -1,25 +1,48 @@
-// Exposing Paths with the pub Keyword
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+// * Exposing Paths with the pub Keyword
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+
+// pub fn eat_at_restaurant() {
+//     // Absolute path
+//     crate::front_of_house::hosting::add_to_waitlist();
+
+//     // Relative path
+//     front_of_house::hosting::add_to_waitlist();
+// }
+
+// * Starting Relative Paths with super
+// fn serve_order() {}
+
+// mod back_of_house {
+//     fn incorrect_order() {
+//         cook_order();
+//         super::serve_order()
+//     }
+//     fn cook_order() {}
+// }
+
+// * Making Structs Public
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
     }
 }
 
 pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
-}
-
-// Starting Relative Paths with super
-fn serve_order() {}
-
-mod back_of_house {
-    fn incorrect_order() {
-        cook_order();
-        super::serve_order()
-    }
-    fn cook_order() {}
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 }
