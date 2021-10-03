84343546dd6e9e2a3ffce9690dcb146569ffeb12
chris_mlvz <chris.mlvz@gmail.com>
2021-10-03 02:29:02 -0500

    String with data

--- the_book/common_collections/strings_utf8/src/main.rs
+++ the_book/common_collections/strings_utf8/src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
     // * Creating a new String
-    let mut s = String::new();
-    
+    // let mut s = String::new();
+
+    // * to_string method
+    // let data = "initial contents".to_string();
+
+    // * String From
+    let s = String::from("initial contents");
 }
