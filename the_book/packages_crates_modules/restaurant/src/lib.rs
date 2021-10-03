a0e3e544c99eee1a62582279139da5ce7970df01
chris_mlvz <chris.mlvz@gmail.com>
2021-10-02 23:08:00 -0500

    Re-exporting Names with pub use

--- the_book/packages_crates_modules/restaurant/src/lib.rs
+++ the_book/packages_crates_modules/restaurant/src/lib.rs
@@ -111,13 +111,28 @@
 // }
 
 // * Providing New Names with the as Keyword
-use std::fmt::Result;
-use std::io::Result as IoResult;
+// use std::fmt::Result;
+// use std::io::Result as IoResult;
 
-fn function1() -> Result {
-    // --snip--
+// fn function1() -> Result {
+//     // --snip--
+// }
+
+// fn function2() -> IoResult<()> {
+//     // --snip--
+// }
+
+// * Re-exporting Names with pub use
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
 }
 
-fn function2() -> IoResult<()> {
-    // --snip--
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
