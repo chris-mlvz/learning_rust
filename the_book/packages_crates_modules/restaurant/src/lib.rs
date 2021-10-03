2aaff8a3ab5ea872b8fcf232da357123ca7e8d75
chris_mlvz <chris.mlvz@gmail.com>
2021-10-02 22:21:18 -0500

    Starting Relative Paths with super

--- the_book/packages_crates_modules/restaurant/src/lib.rs
+++ the_book/packages_crates_modules/restaurant/src/lib.rs
@@ -1,3 +1,4 @@
+// Exposing Paths with the pub Keyword
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -10,4 +11,15 @@ pub fn eat_at_restaurant() {
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+}
+
+// Starting Relative Paths with super
+fn serve_order() {}
+
+mod back_of_house {
+    fn incorrect_order() {
+        cook_order();
+        super::serve_order()
+    }
+    fn cook_order() {}
 }
