90b9e27ec1ad73551c8356a5bd0f9eb43f5e4063
chris_mlvz <chris.mlvz@gmail.com>
2021-10-03 02:49:14 -0500

    Concatenation with the + Operator or the format! Macro

--- the_book/common_collections/strings_utf8/src/main.rs
+++ the_book/common_collections/strings_utf8/src/main.rs
@@ -9,14 +9,31 @@ fn main() {
     // let s = String::from("initial contents");
 
     // * Updating a String
-    let mut s = String::from("foo");
-    s.push_str("bar");
+    // let mut s = String::from("foo");
+    // s.push_str("bar");
 
-    let mut s1 = String::from("foo");
-    let s2 = "bar";
-    s1.push_str(s2);
-    println!("s2 is {}", s2);
+    // let mut s1 = String::from("foo");
+    // let s2 = "bar";
+    // s1.push_str(s2);
+    // println!("s2 is {}", s2);
 
-    let mut s2 = String::from("lo");
-    s.push('l');
+    // let mut s2 = String::from("lo");
+    // s.push('l');
+
+    // * Concatenation with the + Operator or the format! Macro
+    // let s1 = String::from("Hello, ");
+    // let s2 = String::from("world!");
+    // let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+    // println!("{}{}", s2, s3);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+    println!("{}", s);
+
+    // * format macro
+    let format_str = format!("{}-{}-{}", s2, s2, s3);
+    println!("{}", format_str);
 }
