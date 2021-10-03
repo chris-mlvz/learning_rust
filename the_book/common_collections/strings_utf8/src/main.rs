9976046052e4ec469cc94a4f764e5deb55759406
chris_mlvz <chris.mlvz@gmail.com>
2021-10-03 02:34:53 -0500

    Appending to a String with push_str and push

--- the_book/common_collections/strings_utf8/src/main.rs
+++ the_book/common_collections/strings_utf8/src/main.rs
@@ -6,5 +6,17 @@ fn main() {
     // let data = "initial contents".to_string();
 
     // * String From
-    let s = String::from("initial contents");
+    // let s = String::from("initial contents");
+
+    // * Updating a String
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {}", s2);
+
+    let mut s2 = String::from("lo");
+    s.push('l');
 }
