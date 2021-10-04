b07f9250813132b44a578b7cacc4bda34c58c049
chris_mlvz <chris.mlvz@gmail.com>
2021-10-04 14:13:47 -0500

    Creating hash maps

---
+++ the_book/common_collections/hash_maps/src/main.rs
@@ -0,0 +1,20 @@
+// * Hash Maps
+
+// * Creating a New Hash Map
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    println!("{:?}", scores);
+
+    // * Another way of constructing a HashMap
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> = teams.into_iter().zip(initial_scores.into_iter()).collect();
+    println!("{:?}", scores);
+}
