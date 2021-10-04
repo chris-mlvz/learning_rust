47cd4f8f9ea3e64711b2e19c9c54ab21fa2ae0eb
chris_mlvz <chris.mlvz@gmail.com>
2021-10-04 14:48:56 -0500

    Using a panic! Backtrace

--- the_book/error_handling/erros_panic/src/main.rs
+++ the_book/error_handling/erros_panic/src/main.rs
@@ -1,3 +1,8 @@
+// * Unrecoverable Errors with panic!
 fn main() {
-    panic!("crash and burn");
+    // panic!("crash and burn");
+
+    // * Using a panic! Backtrace
+    let v = vec![1, 2, 3];
+    v[99];
 }
