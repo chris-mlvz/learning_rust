2f1ccdbf7d9a4dc61175e2a0a574140798a72688
chris_mlvz <chris.mlvz@gmail.com>
2021-10-04 19:15:24 -0500

    Propagating Errors

--- the_book/error_handling/recoverable_errors/src/main.rs
+++ the_book/error_handling/recoverable_errors/src/main.rs
@@ -50,9 +50,35 @@
 // }
 
 // * Shortcuts for Panic on Error: unwrap and expect
+// use std::fs::File;
+
+// fn main() {
+//     let f = File::open("hello.txt").unwrap();
+//     let f1 = File::open("hello.txt").expect("Failed to open hello.txt");
+// }
+
+// * Propagating Errors
 use std::fs::File;
+use std::io::{self, Read};
 
 fn main() {
-    let f = File::open("hello.txt").unwrap();
-    let f1 = File::open("hello.txt").expect("Failed to open hello.txt");
+    read_username_from_file().unwrap();
 }
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+// The ? operator
