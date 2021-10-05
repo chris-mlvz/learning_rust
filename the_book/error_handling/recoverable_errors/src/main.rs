1a1b4fc45e57faf7ef7ce3e0c31bec42578023ae
chris_mlvz <chris.mlvz@gmail.com>
2021-10-04 19:32:10 -0500

    A Shortcut for Propagating Errors: the ? Operator

--- the_book/error_handling/recoverable_errors/src/main.rs
+++ the_book/error_handling/recoverable_errors/src/main.rs
@@ -58,27 +58,48 @@
 // }
 
 // * Propagating Errors
+// use std::fs::File;
+// use std::io::{self, Read};
+
+// fn main() {
+//     read_username_from_file().unwrap();
+// }
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let f = File::open("hello.txt");
+
+//     let mut f = match f {
+//         Ok(file) => file,
+//         Err(e) => return Err(e),
+//     };
+
+//     let mut s = String::new();
+
+//     match f.read_to_string(&mut s) {
+//         Ok(_) => Ok(s),
+//         Err(e) => Err(e),
+//     }
+// }
+
+// * A Shortcut for Propagating Errors: the ? Operator
 use std::fs::File;
 use std::io::{self, Read};
 
 fn main() {
     read_username_from_file().unwrap();
+    read_username().unwrap();
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let f = File::open("hello.txt");
-
-    let mut f = match f {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-
+    let mut f = File::open("hello.txt")?;
     let mut s = String::new();
-
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => Err(e),
-    }
+    f.read_to_string(&mut s)?;
+    Ok(s)
 }
 
-// The ? operator
+// * more simple
+fn read_username() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello,=.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
