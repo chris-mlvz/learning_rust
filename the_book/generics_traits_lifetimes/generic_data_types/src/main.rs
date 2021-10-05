2e6670fb61e00b9678e087b3a6a2b0a863523a56
chris_mlvz <chris.mlvz@gmail.com>
2021-10-05 14:44:06 -0500

    In Struct Definitions

--- the_book/generics_traits_lifetimes/generic_data_types/src/main.rs
+++ the_book/generics_traits_lifetimes/generic_data_types/src/main.rs
@@ -1,26 +1,65 @@
 // * Generic Data Types
 
 // * In Function Definitions
-fn largest<T>(list: &[T]) -> T {
-    let mut largest = list[0];
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
 
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
+//     for &item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
 
-    largest
+//     largest
+// }
+
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+
+//     let result = largest(&number_list);
+//     println!("The largest number is {}", result);
+
+//     let char_list = vec!['y', 'm', 'a', 'q'];
+
+//     let result = largest(&char_list);
+//     println!("The largest char is {}", result);
+// }
+
+// * In Struct Definitions
+// #[derive(Debug)]
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// fn main() {
+//     let integer = Point { x: 5, y: 10 };
+//     let float = Point { x: 1.0, y: 4.0 };
+//     println!("{:?}", integer);
+//     println!("{:?}", float);
+// }
+
+// ! Don't work
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// fn main() {
+//     let wont_work = Point { x: 5, y: 4.0 };
+// }
+#[derive(Debug)]
+struct Point<T, U> {
+    x: T,
+    y: U,
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+    println!("{:?}", both_integer);
+    println!("{:?}", both_float);
+    println!("{:?}", integer_and_float);
 }
