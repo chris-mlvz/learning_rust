cd7a3d98bc627d5cbbdf4adcb8c9b4fe96999aa8
chris_mlvz <chris.mlvz@gmail.com>
2021-10-06 02:49:55 -0500

    The Borrow Checker

--- the_book/generics_traits_lifetimes/lifetimes/validating_references_with_lifetimes/src/main.rs
+++ the_book/generics_traits_lifetimes/lifetimes/validating_references_with_lifetimes/src/main.rs
@@ -1,14 +1,25 @@
 // * Validating References with Lifetimes
 
-
 // ! Preventing Dangling References with Lifetimes
-fn main() {
-    let r;
+// fn main() {
+//     let r;
 
-    {
-        let x = 5;
-        r = &x;
-    }
+//     {
+//         let x = 5;
+//         r = &x;
+//     }
 
-    println!("r: {}", r);
-}
+//     println!("r: {}", r);
+// }
+
+// * The Borrow Checker
+    // {
+    //     let r;                // ---------+-- 'a
+    //                           //          |
+    //     {                     //          |
+    //         let x = 5;        // -+-- 'b  |
+    //         r = &x;           //  |       |
+    //     }                     // -+       |
+    //                           //          |
+    //     println!("r: {}", r); //          |
+    // }
