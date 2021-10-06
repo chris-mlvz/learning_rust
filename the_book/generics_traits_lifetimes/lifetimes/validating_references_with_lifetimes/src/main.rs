8c67db5abe10f1405178645b5da61a14b84c155c
chris_mlvz <chris.mlvz@gmail.com>
2021-10-06 03:28:58 -0500

    Lifetime Annotations in Function Signatures

--- the_book/generics_traits_lifetimes/lifetimes/validating_references_with_lifetimes/src/main.rs
+++ the_book/generics_traits_lifetimes/lifetimes/validating_references_with_lifetimes/src/main.rs
@@ -26,14 +26,35 @@
 
 // * Generic Lifetimes in Functions
 fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
+    // let string1 = String::from("abcd");
+    // let string2 = "xyz";
 
-    let result = longest(string1.as_str(), string2);
+    // let result = longest(string1.as_str(), string2);
+    // println!("The longest string is {}", result);
+
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
     println!("The longest string is {}", result);
 }
 
-fn longest(x: &str, y: &str) -> &str {
+// fn longest(x: &str, y: &str) -> &str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+// * Lifetime Annotation Syntax
+// &i32        // a reference
+// &'a i32     // a reference with an explicit lifetime
+// &'a mut i32 // a mutable reference with an explicit lifetime
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
