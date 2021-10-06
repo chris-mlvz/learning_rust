94595f8c1ac422eeb087516b8a7d9a9a44dbbc15
chris_mlvz <chris.mlvz@gmail.com>
2021-10-06 02:56:06 -0500

    Generic Lifetimes in Functions

--- the_book/generics_traits_lifetimes/lifetimes/validating_references_with_lifetimes/src/main.rs
+++ the_book/generics_traits_lifetimes/lifetimes/validating_references_with_lifetimes/src/main.rs
@@ -13,13 +13,30 @@
 // }
 
 // * The Borrow Checker
-    // {
-    //     let r;                // ---------+-- 'a
-    //                           //          |
-    //     {                     //          |
-    //         let x = 5;        // -+-- 'b  |
-    //         r = &x;           //  |       |
-    //     }                     // -+       |
-    //                           //          |
-    //     println!("r: {}", r); //          |
-    // }
+// {
+//     let r;                // ---------+-- 'a
+//                           //          |
+//     {                     //          |
+//         let x = 5;        // -+-- 'b  |
+//         r = &x;           //  |       |
+//     }                     // -+       |
+//                           //          |
+//     println!("r: {}", r); //          |
+// }
+
+// * Generic Lifetimes in Functions
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
+
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
