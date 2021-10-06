aeb91f1c2af2d659c5bdc385921e9a29ce8961bf
chris_mlvz <chris.mlvz@gmail.com>
2021-10-06 03:54:19 -0500

    Lifetime Elision

--- the_book/generics_traits_lifetimes/lifetimes/lifetime_structs/src/main.rs
+++ the_book/generics_traits_lifetimes/lifetimes/lifetime_structs/src/main.rs
@@ -10,4 +10,19 @@ fn main() {
         part: first_sentence,
     };
     println!("{:?}", i);
+    let first = first_word(first_sentence);
+    println!("{}", first);
+}
+
+// * Lifetime Elision -  lifetime elision rules.
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
