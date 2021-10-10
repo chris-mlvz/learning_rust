47d7917edf2bb3b90c956de04d890605b4288576
chris-mlvz <chris.mlvz@gmail.com>
2021-10-10 15:43:03 -0500

    Testing Equality with assert_eq and ne! macros

--- the_book/tests/adder/src/lib.rs
+++ the_book/tests/adder/src/lib.rs
@@ -1,42 +1,56 @@
+// * How to Write Tests
+// * The Anatomy of a Test Function
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-        assert!(larger.can_hold(&smaller));
-    }
+    // * Checking Results with the assert! Macro
+    // #[test]
+    // fn larger_can_hold_smaller() {
+    //     let larger = Rectangle {
+    //         width: 8,
+    //         height: 7,
+    //     };
+    //     let smaller = Rectangle {
+    //         width: 5,
+    //         height: 1,
+    //     };
+    //     assert!(larger.can_hold(&smaller));
+    // }
+
+    // #[test]
+    // fn smaller_cannot_hold_larger() {
+    //     let larger = Rectangle {
+    //         width: 8,
+    //         height: 7,
+    //     };
+    //     let smaller = Rectangle {
+    //         width: 8,
+    //         height: 1,
+    //     };
+
+    //     assert!(!smaller.can_hold(&larger));
+    // }
+
+    // * Testing Equality with the assert_eq! and assert_ne! Macros
 
     #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 8,
-            height: 1,
-        };
-
-        assert!(!smaller.can_hold(&larger));
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
     }
 }
 
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
 
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width < other.width && self.height > other.height
-    }
+// impl Rectangle {
+//     fn can_hold(&self, other: &Rectangle) -> bool {
+//         self.width < other.width && self.height > other.height
+//     }
+// }
+
+pub fn add_two(a: i32) -> i32 {
+    a + 3
 }
