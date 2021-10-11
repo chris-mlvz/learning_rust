14be4b3309e4506468ceef7435e85a8a16108b23
chris-mlvz <chris.mlvz@gmail.com>
2021-10-10 19:12:56 -0500

    Running a Subset of Tests by Name

--- the_book/tests/how_test_run/src/lib.rs
+++ the_book/tests/how_test_run/src/lib.rs
@@ -7,25 +7,52 @@
 // $ cargo test -- --test-threads=1
 
 // * Showing Function Output
-// $ cargo test -- --show-output
-fn prints_and_returns_10(a: i32) -> i32 {
-    println!("I got the value {}", a);
-    10
+// ? $ cargo test -- --show-output
+// fn prints_and_returns_10(a: i32) -> i32 {
+//     println!("I got the value {}", a);
+//     10
+// }
+
+// #[cfg(test)]
+// mod test {
+//     use super::*;
+
+//     #[test]
+//     fn this_test_will_pass() {
+//         let value = prints_and_returns_10(4);
+//         assert_eq!(10, value);
+//     }
+
+//     #[test]
+//     fn this_test_will_fail() {
+//         let value = prints_and_returns_10(8);
+//         assert_eq!(5, value);
+//     }
+// }
+
+// * Running a Subset of Tests by Name
+// $ cargo test one_hundred
+// $ cargo test add
+pub fn add_two(a: i32) -> i32 {
+    a + 2
 }
 
 #[cfg(test)]
-mod test {
+mod tests {
     use super::*;
 
     #[test]
-    fn this_test_will_pass() {
-        let value = prints_and_returns_10(4);
-        assert_eq!(10, value);
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
     }
 
     #[test]
-    fn this_test_will_fail() {
-        let value = prints_and_returns_10(8);
-        assert_eq!(5, value);
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
     }
 }
