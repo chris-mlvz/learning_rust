58a800184038b0125b4501522a5798139f5fd034
chris-mlvz <chris.mlvz@gmail.com>
2021-10-12 15:16:42 -0500

    Refactoring Using Functions

--- the_book/functional_language_features/closures/src/main.rs
+++ the_book/functional_language_features/closures/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
 
-    generate_workouts(simulated_user_specified_value, simulated_random_number)
+    generate_workout(simulated_user_specified_value, simulated_random_number)
 }
 
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
@@ -14,24 +14,17 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
     intensity
 }
 
-fn generate_workouts(intensity: u32, random_number: u32) {
+fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_result = simulated_expensive_calculation(intensity);
+
     if intensity < 25 {
-        println!(
-            "Today, do {} pushups!",
-            simulated_expensive_calculation(intensity)
-        );
-        println!(
-            "Next, do {} situps!",
-            simulated_expensive_calculation(intensity)
-        );
+        println!("Today, do {} pushups!", expensive_result);
+        println!("Next, do {} situps!", expensive_result);
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!(
-                "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
-            );
+            println!("Today, run for {} minutes!", expensive_result);
         }
     }
 }
