b56addb38d466fbcec59d6cf7c397e7a0762a0b9
chris-mlvz <chris.mlvz@gmail.com>
2021-10-12 15:28:29 -0500

    Closure Type Inference and Annotation

--- the_book/functional_language_features/closures/src/main.rs
+++ the_book/functional_language_features/closures/src/main.rs
@@ -26,3 +26,9 @@ fn generate_workout(intensity: u32, random_number: u32) {
         }
     }
 }
+
+// * Closures, syntax
+// fn  add_one_v1   (x: u32) -> u32 { x + 1 }
+// let add_one_v2 = |x: u32| -> u32 { x + 1 };
+// let add_one_v3 = |x|             { x + 1 };
+// let add_one_v4 = |x|               x + 1  ;
